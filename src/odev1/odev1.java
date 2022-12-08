f1cae92e790c39bf7baa472bd89a21dc79828404
YUSUF <YUSUF@192.168.1.140>
2022-12-08 15:46:45 +0300

    first project

---
+++ src/odev1/odev1.java
@@ -0,0 +1,17 @@
+package odev1;
+
+public class odev1 {
+
+	public static void main(String[] args) {
+	//	1 den 100 e kadar olan çift sayıları ekrana yazdıran program kodu	
+	for(int a= 1; a<=100; a++)
+	{
+		if(a%2==0)
+		{
+			System.out.println(a);
+		}
+	}
+
+	}
+
+}
